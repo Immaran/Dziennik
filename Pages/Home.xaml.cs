68911c6c4b9b60805eb78e32220d4fb86376d029
Bart-coding <bartekpoczta54@gmail.com>
2021-01-26 23:54:56 +0100

    added animation of text in homepage

--- Pages/Home.xaml.cs
+++ Pages/Home.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
 
 namespace SBD.Pages
 {
@@ -11,6 +14,12 @@ namespace SBD.Pages
         public Home()
         {
             InitializeComponent();
+            /*ColorAnimation animation = new ColorAnimation();
+            animation.From = Colors.Black;
+            animation.To = Colors.White;
+            animation.Duration = new Duration(TimeSpan.FromSeconds(1));
+            animation.RepeatBehavior = RepeatBehavior.Forever;
+            this.homeLabel.Foreground.BeginAnimation(SolidColorBrush.ColorProperty, animation);*/
         }
         private void onClick(object sender, EventArgs e)
         {
